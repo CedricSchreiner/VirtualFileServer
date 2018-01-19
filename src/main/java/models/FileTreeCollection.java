8edf16703c4a8b24f20477fb513c671c6f34fd09
weedric <cedi.schreiner@web.de>
2018-01-19 15:53:09 +0100

    FileTreeCollection hinzugefügt. Diese enthält alle FileTrees von allen Usern die momentan auf dem Server angemeldet sind.
    UserTree enthält von jedem User den dazugehörigen Baum

---
+++ src/main/java/models/FileTreeCollection.java
@@ -0,0 +1,38 @@
+package models;
+
+import fileTree.interfaces.TreeInterface;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class FileTreeCollection {
+    private static FileTreeCollection ourInstance = new FileTreeCollection();
+    private Collection<UserTree> gco_treeCollection;
+
+    public static FileTreeCollection getInstance() {
+        return ourInstance;
+    }
+
+    private FileTreeCollection() {
+        this.gco_treeCollection = new ArrayList<>();
+    }
+
+    public void addTreeToCollection(UserTree iob_userTree) {
+        this.gco_treeCollection.add(iob_userTree);
+    }
+
+    public TreeInterface getTreeFromUser(User iob_user) {
+        //--------------------Variables--------------------
+        String lob_userEmail = iob_user.getEmail();
+        String lob_userTreeEmail;
+        //-------------------------------------------------
+
+        for(UserTree userTree : this.gco_treeCollection) {
+            lob_userTreeEmail = userTree.getUser().getEmail();
+            if (lob_userEmail.equals(lob_userTreeEmail)) {
+                return userTree.getTree();
+            }
+        }
+        return null;
+    }
+}
