66bf695581e1e2e8359b4948fd817eeb67e264fc
FlorianH123 <f.hennrich@gmx.de>
2018-01-21 21:35:54 +0100

    User Service implementiert

--- src/main/java/builder/DaoObjectBuilder.java
+++ src/main/java/builder/DaoObjectBuilder.java
@@ -4,6 +4,10 @@ import dao.classes.UserDaoImpl;
 import dao.interfaces.UserDao;
 
 public class DaoObjectBuilder {
+    /**
+     * Generates an UserDao object
+     * @return UserDaoImpl
+     */
     public static UserDao getUserDaoObject() {
         return new UserDaoImpl();
     }
--- src/main/java/builder/ServiceObjectBuilder.java
+++ src/main/java/builder/ServiceObjectBuilder.java
@@ -4,6 +4,10 @@ import services.classes.UserServiceImpl;
 import services.interfaces.UserService;
 
 public class ServiceObjectBuilder {
+    /**
+     * Generates an UserService object
+     * @return UserServiceImpl
+     */
     public static UserService getUserServiceObject() {
         return new UserServiceImpl();
     }
