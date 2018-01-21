86d170e0e69e2fc87f464c01728ed5be1cfc3c7e
FlorianH123 <f.hennrich@gmx.de>
2018-01-21 21:43:28 +0100

    User Service implementiert

--- src/main/java/dao/constants/DaoConstants.java
+++ src/main/java/dao/constants/DaoConstants.java
@@ -4,6 +4,4 @@ public abstract class DaoConstants {
     public static final int PARAMETER_1 = 1;
     public static final int PARAMETER_2 = 2;
     public static final int PARAMETER_3 = 3;
-    public static final int PARAMETER_4 = 4;
-
 }
