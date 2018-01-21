7bdc388a14251312503570a69ba0ee8d5c0753d1
FlorianH123 <f.hennrich@gmx.de>
2018-01-21 19:19:21 +0100

    Packages überarbeitet + Dao Schnittstelle

--- src/main/java/dao/interfaces/UserDao.java
+++ src/main/java/dao/interfaces/UserDao.java
@@ -5,7 +5,6 @@ import models.interfaces.User;
 import java.util.List;
 
 public interface UserDao {
-
     /**
      * get a user from the database with a specific email
      * @param iva_email get the user with this email
@@ -13,7 +12,12 @@ public interface UserDao {
      */
     User getUser(String iva_email);
 
+    /**
+     * Get all users from db
+     * @return all user
+     */
     List<User> getAllUsers();
+
     /**
      * delete a user from the database
      * @param user delete that specific user
@@ -29,11 +33,26 @@ public interface UserDao {
     boolean createUser(User user);
 
     /**
-     * update an user in the database
+     * Update the e-mail of an user
+     *
      * @param user update this user
      * @return false if an error occurred, otherwise true
      */
     boolean updateEmail(User user);
+
+    /**
+     * Update the password of an user
+     *
+     * @param user update this user
+     * @return false if an error occurred, otherwise true
+     */
     boolean updatePassword(User user);
+
+    /**
+     * Update the name of an user
+     *
+     * @param user update this user
+     * @return false if an error occurred, otherwise true
+     */
     boolean updateName(User user);
 }
