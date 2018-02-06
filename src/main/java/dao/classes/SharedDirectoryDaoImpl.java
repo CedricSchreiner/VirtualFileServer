2cbd144595dec00c9ae834ba386773cdd09d6239
FlorianH123 <f.hennrich@gmx.de>
2018-02-06 16:28:57 +0100

    SharedDirectory

--- src/main/java/dao/classes/SharedDirectoryDaoImpl.java
+++ src/main/java/dao/classes/SharedDirectoryDaoImpl.java
@@ -2,6 +2,7 @@ package dao.classes;
 
 import builder.DaoObjectBuilder;
 import builder.ModelObjectBuilder;
+import dao.enums.ColNameSharedDirectory;
 import dao.interfaces.SharedDirectoryDao;
 import models.interfaces.SharedDirectory;
 import models.interfaces.User;
@@ -9,6 +10,7 @@ import utilities.Utils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,6 +35,19 @@ public class SharedDirectoryDaoImpl implements SharedDirectoryDao {
 
     private static final String GC_REMOVE_MEMBER_FROM_SHARED_DIRECTORY = "DELETE FROM " +
             TABLE_SHARED_DIRECTORY_MEMBER + " WHERE " + COL_SHARED_D_MEMBER_MEMBER + " = ? AND " +
+            COL_SHARED_D_MEMBER_GROUP_ID + " = ?";
+
+    /**
+     * SELECT * FROM SharedDirectory LEFT OUTER JOIN SharedDirectoryMember ON SharedDirectory.id = SharedDirectoryMember.groupId
+     */
+    private static final String GC_GET_ALL_SHARED_DIRECTORIES = "SELECT * FROM " + TABLE_SHARED_DIRECTORY +
+            " LEFT OUTER JOIN " + TABLE_SHARED_DIRECTORY_MEMBER + " ON " + TABLE_SHARED_DIRECTORY + "." +
+            COL_SHARED_D_ID + " = " + TABLE_SHARED_DIRECTORY_MEMBER + "." + COL_SHARED_D_MEMBER_GROUP_ID;
+
+    /**
+     * DELETE FROM SharedDirectoryMember WHERE groupId = ?
+     */
+    private static final String GC_REMOVE_MEMBER = "DELETE FROM " + TABLE_SHARED_DIRECTORY_MEMBER + " WHERE " +
             COL_SHARED_D_MEMBER_GROUP_ID + " = ?";
 // ---------------------------------------------------------------------------------------------------------------------
 
@@ -76,6 +91,16 @@ public class SharedDirectoryDaoImpl implements SharedDirectoryDao {
         int lva_rowCount = 0;
 
         try (Connection lob_connection = this.gob_databaseConnection.getConnection();
+             PreparedStatement lob_preparedStatement = lob_connection.prepareStatement(GC_REMOVE_MEMBER)) {
+
+            lob_preparedStatement.setInt(PARAMETER_1, iob_sharedDirectory.getId());
+
+            lva_rowCount = lob_preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        try (Connection lob_connection = this.gob_databaseConnection.getConnection();
              PreparedStatement lob_preparedStatement = lob_connection.prepareStatement(GC_DELETE_SHARED_DIRECTORY)) {
 
             lob_preparedStatement.setInt(PARAMETER_1, iob_sharedDirectory.getId());
@@ -108,25 +133,52 @@ public class SharedDirectoryDaoImpl implements SharedDirectoryDao {
 
     @Override
     public List<SharedDirectory> getAllSharedDirectories() {
-        return null;
-    }
+        List<SharedDirectory> lob_sharedDirectoryList = new ArrayList<>();
+        List<User> lob_memberList = new ArrayList<>();
 
-    public static void main(String[] args) {
-        SharedDirectoryDao s = DaoObjectBuilder.getSharedDirectoryObject();
+        ResultSet lob_rs;
+        SharedDirectory lob_sharedDirectory;
+        User lob_owner;
+        User lob_member;
+        int lva_sharedDirectoryID;
+        boolean lva_sharedDirectoryExists;
 
-        SharedDirectory sh = ModelObjectBuilder.getSharedDirectoryObject();
-        User owner = ModelObjectBuilder.getUserModel();
-        owner.setUserId(3);
-        User member = ModelObjectBuilder.getUserModel();
-        member.setUserId(5);
-        ArrayList<User> userList = new ArrayList<>();
-        userList.add(member);
-        sh.setDirectoryName("testOrdner");
-        sh.setOwner(owner);
-        sh.setId(1);
-        sh.setMembers(userList);
+        try (Connection lob_connection = this.gob_databaseConnection.getConnection();
+             PreparedStatement lob_preparedStatement = lob_connection.prepareStatement(GC_GET_ALL_SHARED_DIRECTORIES)) {
 
-        s.removeMemberFromSharedDirectory(sh, member);
+            lob_rs = lob_preparedStatement.executeQuery();
 
+
+            while (lob_rs.next()) {
+                lob_sharedDirectory = ModelObjectBuilder.getSharedDirectoryObject();
+                lob_member = ModelObjectBuilder.getUserModel();
+                lob_owner = ModelObjectBuilder.getUserModel();
+                lva_sharedDirectoryExists = false;
+
+                lva_sharedDirectoryID = lob_rs.getInt(COL_SHARED_D_ID);
+                for (SharedDirectory lob_tmp : lob_sharedDirectoryList) {
+                    if (lob_tmp.getId() == lva_sharedDirectoryID) {
+                        lob_member.setUserId(lob_rs.getInt(COL_SHARED_D_MEMBER_MEMBER));
+                        lob_tmp.getMembers().add(lob_member);
+                        lva_sharedDirectoryExists = true;
+                    }
+                }
+
+                if (!lva_sharedDirectoryExists) {
+                    lob_sharedDirectory.setId(lob_rs.getInt(COL_SHARED_D_ID));
+                    lob_owner.setUserId(lob_rs.getInt(COL_SHARED_D_OWNER));
+                    lob_sharedDirectory.setOwner(lob_owner);
+                    lob_sharedDirectory.setDirectoryName(lob_rs.getString(COL_SHARED_D_GROUP_NAME));
+                    lob_member.setUserId(lob_rs.getInt(COL_SHARED_D_MEMBER_MEMBER));
+                    lob_memberList.add(lob_member);
+                    lob_sharedDirectory.setMembers(lob_memberList);
+                    lob_sharedDirectoryList.add(lob_sharedDirectory);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lob_sharedDirectoryList;
     }
 }
