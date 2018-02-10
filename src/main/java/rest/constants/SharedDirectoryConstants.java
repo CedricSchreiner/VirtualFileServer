3b27ce6d5ecde95fc94b57183d13528546e74daa
FlorianH123 <f.hennrich@gmx.de>
2018-02-10 21:02:13 +0100

    VFS Root Directory

--- src/main/java/rest/constants/SharedDirectoryConstants.java
+++ src/main/java/rest/constants/SharedDirectoryConstants.java
@@ -6,12 +6,12 @@ public abstract class SharedDirectoryConstants {
 // Resource paths
 // ---------------------------------------------------------------------------------------------------------------------
 
-    public static final String GC_SHARED_DIRECTORY_BASE_PATH = "sharedDirectory/auth";
-    public static final String GC_GET_ALL_SHARED_DIRECTORIES_FROM_USER = "getAllSharedDirectoriesFromUser/";
-    public static final String GC_ADD_NEW_SHARED_DIRECTORY = "addNewSharedDirectory";
-    public static final String GC_ADD_NEW_MEMBER_TO_SHARED_DIRECTORY = "addNewMemberToSharedDirectory";
-    public static final String GC_DELETE_SHARED_DIRECTORY = "deleteSharedDirectory";
-    public static final String GC_REMOVE_MEMBER_FROM_SHARED_DIRECTORY = "removeMemberFromSharedDirectory";
+    public static final String GC_SHARED_DIRECTORY_BASE_PATH            = "sharedDirectory/auth";
+    public static final String GC_GET_ALL_SHARED_DIRECTORIES_FROM_USER  = "getAllSharedDirectoriesFromUser/";
+    public static final String GC_ADD_NEW_SHARED_DIRECTORY              = "addNewSharedDirectory";
+    public static final String GC_ADD_NEW_MEMBER_TO_SHARED_DIRECTORY    = "addNewMemberToSharedDirectory";
+    public static final String GC_DELETE_SHARED_DIRECTORY               = "deleteSharedDirectory";
+    public static final String GC_REMOVE_MEMBER_FROM_SHARED_DIRECTORY   = "removeMemberFromSharedDirectory";
 
 // ---------------------------------------------------------------------------------------------------------------------
 }
