f346d5288921a92b96f11038c656f79931ca7463
FlorianH123 <f.hennrich@gmx.de>
2018-02-10 14:46:37 +0100

    SharedDirectory

--- src/main/java/rest/resourcess/AdminResource.java
+++ src/main/java/rest/resourcess/AdminResource.java
@@ -9,6 +9,8 @@ import services.interfaces.AdminService;
 
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static rest.constants.AdminResourceConstants.GC_ADD_ADMIN_STATUS;
@@ -17,6 +19,7 @@ import static rest.constants.AdminResourceConstants.GC_ADMIN_SUCCESSFULLY_ADDED;
 import static rest.resourcess.AdminResource.ADMIN_PATH;
 
 @Path(ADMIN_PATH)
+@Produces(MediaType.APPLICATION_JSON)
 public class AdminResource {
     static final String ADMIN_PATH = "/admin";
     private static final String ADD_NEW_ADMIN_PATH = "/adminAuth/addNewAdmin";
