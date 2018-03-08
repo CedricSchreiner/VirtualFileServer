dd62fc66fa0759a2287eb36c6c8ff03148c07c75
FlorianH123 <f.hennrich@gmx.de>
2018-03-08 20:49:17 +0100

    VFS Root Directory

--- src/main/java/rest/constants/RestConstants.java
+++ src/main/java/rest/constants/RestConstants.java
@@ -1,8 +1,7 @@
 package rest.constants;
 
 public abstract class RestConstants {
-    // TODO Toller Variablenname ausdenken :P
-    public static final String GC_USERS = "Users are not the same!";
+    public static final String GC_USERS = "You are not allowed to access this resource!";
 
     public static final String GC_USER_URI = "auth";
     public static final String GC_ADMIN_URI = "adminAuth";
